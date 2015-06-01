cf6b6df0db5a650f1e939dda1841c6ce3b737990
gillsonkell <gillsonkell@gmail.com>
2015-06-01 10:00:05 -0500

    Add process to empty VDATA directories

--- pre-backup.bat
+++ pre-backup.bat
@@ -2,14 +2,24 @@
 SETLOCAL EnableDelayedExpansion
 SET "updatePackage=https://github.com/gillsonkell/novabackup-maintenance-scripts/archive/master.zip"
 SET "updatePackageFolder=novabackup-maintenance-scripts-master"
+SET vdataLocations="C:\kdr\vdata" "D:\kdr\vdata"
 
 REM ### Update Scripts ###
 IF NOT [%1] == [no-update] (
 	curl -o update.zip -L -k %updatePackage%
 	7z x update.zip
 	XCOPY .\%updatePackageFolder%\* .\ /C /Q /S /E /H /Y
-    RMDIR /S /Q %updatePackageFolder%
+    RD /S /Q %updatePackageFolder%
     DEL update.zip
 	CALL pre-backup.bat no-update
 	EXIT /B
+)
+
+REM ### Empty VDATA Folders ###
+FOR %%f IN (%vdataLocations%) DO (
+	IF EXIST %%f (
+		ECHO Emptying %%f...
+		RD /S /Q %%f
+		MD %%f
+	)
 )
